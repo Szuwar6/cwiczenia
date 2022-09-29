5267c116eb82686a9c02587809e63fd75f3786f0
Szuwar <marekszubartowicz@gmail.com>
2022-09-29 21:33:46 +0200

    kolejny

--- po hr.py
+++ po hr.py
@@ -1,5 +1,7 @@
 import string
 import re
+from math import sqrt
+
 
 def is_pangram(s):
 
@@ -91,3 +93,33 @@ def czy_pierwsza(n):
 
 print(czy_pierwsza(1))
 
+def is_prime(n):
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+class PrimeIterator:
+    # Iterator pozwalający na iterowanie po n liczbach pierwszych
+    def __init__(self, n):
+        self.n = n
+        self.generated_numbers = 0
+        self.number = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.number += 1
+        if self.generated_numbers >= self.n:
+            raise StopIteration
+        elif is_prime(self.number):
+            self.generated_numbers += 1
+            return self.number
+        return self.__next__()
+
+
+iter = PrimeIterator(100)
+for elem in iter:
+    print(elem)
