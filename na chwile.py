6eca725693a6a2eb46e83e4433e29d25810d0c87
Szuwar <marekszubartowicz@gmail.com>
2022-10-02 15:07:52 +0200

    kolejny

--- na chwile.py
+++ na chwile.py
@@ -1,13 +1,20 @@
-def sum_of_differences(arr):
-    return max(arr) - min(arr) if arr else 0
-    # if len(arr) < 2:
-    #     return 0
-    # s = sorted(arr)[::-1]
-    # sum = 0
-    # for i in range(1, len(s)):
-    #     sum += s[i-1] - s[i]
-    #
-    # return sum
+class Parent1:
+    def __init__(self):
+        super().__init__()
+        print("In parent 1 class...")
 
+class Parent2:
+    def __init__(self):
+        print("In parent 2 class...")
 
-print(sum_of_differences([-3,-2,-1, -1]))
+class Derived(Parent1, Parent2):
+    def __init__(self):
+        super().__init__()
+        print("In derived class...")
+
+def main():
+    d = Derived()
+
+if __name__ == '__main__':
+    main()
+    print(Derived.mro())
